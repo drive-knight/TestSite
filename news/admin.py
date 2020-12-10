8344706634a46faedb16db81324ae00fd67931b1
Ivan Novikov <johanxvcii@gmail.com>
2020-12-11 00:05:58 +0500

    add app

---
+++ news/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+from .models import News, Category
+from django import forms
+
+
+
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
+
+class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo','views', 'created_at', 'updated_at')
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" widht="50" height="50">')
+        else:
+            return 'Нет фотографии'
+
+    get_photo.short_description = 'Фотография'
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.site_title = 'Управление новостями'
+admin.site.site_headet = 'Управление новостями'
